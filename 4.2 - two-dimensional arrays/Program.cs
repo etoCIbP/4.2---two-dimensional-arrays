d38f21d7d61b1e6d6daa4b7bc4cb28c169293f10
Teya <Teya@TVOROG>
2021-12-23 04:25:40 +0200

    Добавьте файлы проекта.

---
+++ 4.2 - two-dimensional arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4._2___two_dimensional_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massiv1 = new int[30], massiv2 = new int[30], massiv3 = new int[30]; 
+            Random random1 = new Random(), random2 = new Random(), random3 = new Random();
+            //int[] massiv2 = new int[30];
+            //Random random2 = new Random();
+
+            for (int i = 0; i < 10; i++)
+
+            {
+                massiv1[i] = random1.Next(0, 9)*3*-1;   //Берутся числа от 0 до 9
+                massiv2[i] = random2.Next(0, 9)*3*-1;   // Затем умножаются на 3
+                massiv3[i] = random3.Next(0, 9)*3*-1;   // и далее умножаются на -1 для отрицательного числа
+                if (massiv1[i] % 2 == 0)
+                    Console.WriteLine("{0} Первый массив: ", massiv1[i]);
+                //Console.WriteLine(" ");
+                    Console.WriteLine("{0} Второй массив: ", massiv2[i]);
+                //Console.WriteLine(" ");
+                    Console.WriteLine("{0} Третий массив: ", massiv3[i]);
+                //Console.WriteLine(" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
